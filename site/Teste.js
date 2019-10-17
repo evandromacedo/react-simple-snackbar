c00284f0078bde1c60828a4d49144817b05570c3
Evandro Macedo <evandro.hcm@gmail.com>
2019-10-17 03:01:09 -0300

    Creates Header on site

--- site/Teste.js
+++ site/Teste.js
@@ -5,7 +5,11 @@ export default function Teste() {
   const [open, close] = useSnackbar()
 
   return (
-    <button onClick={() => open('To achando esse texto da snackbar tão apagado agora!')}>
+    <button
+      onClick={() =>
+        open('To achando esse texto da snackbar tão apagado agora!', 3000000)
+      }
+    >
       Trigger Snackbar
     </button>
   )
