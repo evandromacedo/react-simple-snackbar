9ee972cddde6daff8a8e97a509d36c2c4d7259e5
Evandro Macedo <evandro.hcm@gmail.com>
2019-10-17 23:36:22 -0300

    Change some styles

--- site/Teste.js
+++ site/Teste.js
@@ -2,7 +2,11 @@ import React from 'react'
 import { useSnackbar } from 'react-simple-snackbar'
 
 export default function Teste() {
-  const [open, close] = useSnackbar()
+  const [open, close] = useSnackbar({
+    style: {
+      backgroundImage: 'linear-gradient(to bottom right, #00dafc, #2e62b7, #540055)',
+    },
+  })
 
   return (
     <button
