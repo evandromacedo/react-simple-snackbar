28ad2712ce4597e674441d3aa2096ed4bf0b35b5
Evandro Macedo <evandro.hcm@gmail.com>
2019-10-17 23:45:01 -0300

    Change theme of code blocks

--- site/Teste.js
+++ site/Teste.js
@@ -2,9 +2,9 @@ import React from 'react'
 import { useSnackbar } from 'react-simple-snackbar'
 
 export default function Teste() {
-  const [open, close] = useSnackbar({
+  const [open] = useSnackbar({
     style: {
-      backgroundImage: 'linear-gradient(to bottom right, #00dafc, #2e62b7, #540055)',
+      backgroundImage: 'linear-gradient(to bottom right, #00b3f1, #2e62b7, #540055)',
     },
   })
 
