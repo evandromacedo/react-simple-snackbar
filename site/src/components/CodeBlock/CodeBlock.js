490d3fe3c91dbcdd975ec20bec11c8f7c36431d8
Evandro Macedo <evandro.hcm@gmail.com>
2019-10-21 19:02:54 -0300

    Change theme

--- site/src/components/CodeBlock/CodeBlock.js
+++ site/src/components/CodeBlock/CodeBlock.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Highlight, { defaultProps } from 'prism-react-renderer'
-import theme from 'prism-react-renderer/themes/shadesOfPurple'
+import theme from 'prism-react-renderer/themes/github'
 
 export const LineNumber = {
   display: 'inline-block',
